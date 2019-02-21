182ec0a19eeffaf451866515c284a7dde561b6d3
Ashish Garg <mragarg@gmail.com>
2019-02-21 09:10:25 -0500

    Opens game with the uploaded background image

--- monster.py
+++ monster.py
@@ -1,34 +1,42 @@
 import pygame
 
 def main():
-    width = 500
-    height = 500
-    background_iamge = pygame.image.load("background.png")
-
-    #initialize pygame
+    # Initialize pygame
     pygame.init()
-    #set up the screen (drawing surface for the game)
-    screen = pygame.display.set_mode((width, height))
-    #setting a caption
-    pygame.display.set_caption('My Game')
+
+    # Importing background image
+    background_iamge = pygame.image.load("./images/background.png")
+
+    # Window size
+    window_width = 512
+    window_height = 480
+
+    # Set up the screen (drawing surface for the game)
+    screen = pygame.display.set_mode((window_width, window_height))
+    
+    # Game Window Title
+    pygame.display.set_caption('Monster Game')
+    
+    # Calls the Clock object and sets it to variable clock (for shorthand purposes)
     clock = pygame.time.Clock()
 
     # Game initialization
 
     stop_game = False
     while not stop_game:
+
+        # Event is for user input such as keypress to clicks
         for event in pygame.event.get():
 
             # Event handling
-
             if event.type == pygame.QUIT:
                 stop_game = True
 
 
         # Game logic
 
-        # Draw background
-        screen.fill(background_iamge)
+        # Uses the uploaded background image as the game background
+        screen.blit(background_iamge, [0, 0])
 
         # Game display
 
